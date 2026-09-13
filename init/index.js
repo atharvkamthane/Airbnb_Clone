23db61f2d3e7aab01b846f474b8e43c17e557be1
atharvkamthane <atharvkamthane@gmail.com>
2026-09-13 14:36:17 +0530

    feat: add initial sample listings and database initialization logic

---
+++ init/index.js
@@ -0,0 +1,27 @@
+const mongoose=require("mongoose");
+
+const initData=require("./data.js");
+const Listing=require("../models/listing.js");
+
+async function main(){
+    await mongoose.connect('mongodb://127.0.0.1:27017/airbnb');
+
+}
+
+main().then(()=>{
+    console.log("database connected")
+}).catch(err=>{
+    console.log(err);
+})
+
+
+
+const initDB=async ()=>{
+    await Listing.deleteMany({});
+    await Listing.insertMany(initData.data);
+    console.log("data was inintialised");
+
+
+}
+
+initDB().then(()=>{console.log("intialised db")}).catch((err)=>{console.log(err)});
